3017234edaccc0d865b4f382c907259051c0b013
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-09-16 08:50:59 +0700

    396: add some users and test

--- src/main/java/com/spring/security/config/SecurityConfig.java
+++ src/main/java/com/spring/security/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 
 /**
  *
@@ -20,7 +21,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(users.username("john").password("123").roles("EMPLOYEE"))
+                .withUser(users.username("mary").password("123").roles("MANAGER"))
+                .withUser(users.username("susan").password("123").roles("ADMIN"));
     }
 
 
